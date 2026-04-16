5a816cef9070e100746802b4178d9ae3dcd14ead
Mayank Raj <171077513+mayankraj9999m@users.noreply.github.com>
2026-04-16 20:12:31 +0530

    Updated server file

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ app.use("/api/admin/chief", chiefWardenRoutes);
 app.use("/api/admin/warden", wardenRoutes);
 app.use('/api/complaints', complaintRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
 export default app; 
