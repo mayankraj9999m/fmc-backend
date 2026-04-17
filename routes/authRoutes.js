68a6594761205a609b82554c314163f165e06afb
Mayank Raj <171077513+mayankraj9999m@users.noreply.github.com>
2026-04-17 11:00:40 +0530

    update 3

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -28,7 +28,7 @@ const generateTokenAndSetCookie = (res, user, role) => {
     res.cookie("token", token, {
         httpOnly: true, // Prevents JavaScript (XSS) from reading the cookie
         secure: process.env.NODE_ENV === "production", // HTTPS only in production
-        sameSite: "lax", // Protects against Cross-Site Request Forgery (CSRF)
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Protects against Cross-Site Request Forgery (CSRF)
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     });
 };
