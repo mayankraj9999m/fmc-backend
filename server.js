13b673f1d0e0fe47fe5257e1f392b1c4649d177d
Mayank Raj <171077513+mayankraj9999m@users.noreply.github.com>
2026-04-17 10:00:48 +0530

    Updated @17-04-2026

--- server.js
+++ server.js
@@ -28,8 +28,4 @@ app.use("/api/admin/chief", chiefWardenRoutes);
 app.use("/api/admin/warden", wardenRoutes);
 app.use('/api/complaints', complaintRoutes);
 
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
-}
-
-export default app; 
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
